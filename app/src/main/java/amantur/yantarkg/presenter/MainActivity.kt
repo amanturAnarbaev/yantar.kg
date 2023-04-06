69e3343efa1225818c12bd3629dff10b7bf436d4
swaaage <arsenjanybaev@gmail.com>
2023-04-06 07:39:17 +0600

    Initial commit

---
+++ app/src/main/java/amantur/yantarkg/presenter/MainActivity.kt
@@ -0,0 +1,12 @@
+    package amantur.yantarkg.presenter
+
+import amantur.yantarkg.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
